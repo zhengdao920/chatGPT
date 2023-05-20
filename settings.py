6ad16fb6126f6ea8fba6081eea545653a40ddf21
政道 <121273569+zhengdao920@users.noreply.github.com>
2023-05-20 14:26:38 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "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"
 
 # openAi 官方 api
 # URL = "https://api.openai.com/v1/chat/completions"
